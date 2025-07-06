3e38ef449164aca29fef78d86fd9973be7e47809
aloui_houssem <aloui9861@gmail.com>
2025-07-06 20:47:54 +0200

    config server registery & service a

---
+++ service-a/src/main/java/com/houssem/service_a/hello/helloController.java
@@ -0,0 +1,15 @@
+package com.houssem.service_a.hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class helloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Service A";
+    }
+}
